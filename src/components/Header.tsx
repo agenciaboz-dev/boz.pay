58e6bacb6223cde49da69950c18c39712f52cccb
nandobfer <nandobfer@yahoo.com.br>
2023-10-08 21:59:25 -0300

    feat: added header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import logo from '../assets/logo.svg'
+import colors from "../style/colors"
 
-interface HeaderProps {
-    
-}
+interface HeaderProps {}
 
-export const Header:React.FC<HeaderProps> = ({  }) => {
-    
+export const Header: React.FC<HeaderProps> = ({}) => {
     return (
-        <Box sx={{width: '100vw', justifyContent: 'center', alignItems: 'center', height: '10vw', borderBottom: '1px solid #C8C9CD'}}>
-            <img src={logo} alt="Boz" style={{width: '10vw'}} />
+        <Box
+            sx={{
+                width: "100vw",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "7vw",
+                borderBottom: `1px solid ${colors.border}`,
+                bgcolor: "white",
+            }}
+        >
+            <img src={logo} alt="Boz" style={{ width: "7vw" }} />
         </Box>
     )
 }
