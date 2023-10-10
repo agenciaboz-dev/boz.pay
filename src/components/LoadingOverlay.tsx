be2b4b867bb7c28540ccffcdbff94250574e30a4
nandobfer <nandobfer@yahoo.com.br>
2023-10-10 19:00:52 -0300

    update: loading circle

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -29,7 +29,7 @@ export const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ open }) => {
         >
             <Box sx={{ position: "relative" }}>
                 <LockIcon sx={{ width: "7vw", height: "7vw" }} />
-                <CircularProgress size={"9vw"} sx={{ position: "absolute", top: "-1vw", left: "-1vw" }} />
+                <CircularProgress size={"10vw"} sx={{ position: "absolute", top: "-1.5vw", left: "-1.5vw" }} />
             </Box>
             <h2>Aguarde um instante, seu pagamento está sendo processado com segurança...</h2>
             <h2 style={{ fontWeight: "normal", color: "black" }}>Por favor, não feche ou recarregue esta página.</h2>
