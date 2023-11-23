e7852a7d08d781cdb0da59561c8e1fac79907eef
nandobfer <nandobfer@yahoo.com.br>
2023-11-23 15:30:01 -0300

    update: new deploy server

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ subdomain="pay.agenciaboz.com.br"
 
 path="/home/${domain}/${subdomain}"
 
-yarn build
+npx vite build
 echo 'Uploading build to server'
 scp -r dist/* ${ssh_profile}:${path}
 ssh ${ssh_profile} "chown -R ${user}:${user} ${path}/*"
